af011b318baea074c9e4dd32aad129f4b270b782
gubanovpm <petrgubanov37@gmail.com>
2023-05-15 00:56:46 +0300

    very important commit

--- BBS.v
+++ BBS.v
@@ -92,3 +92,55 @@ begin
 end
 
 endmodule
+
+module hex2digit_hex
+#(
+	parameter INVERT = 1
+)
+(
+	input	wire	[3:0]	hex,
+	output	wire	[6:0]	digit
+);
+
+	wire [6:0] temp;
+	assign temp =  ({7{hex == 4'h0}} & 7'b1000000 |
+					{7{hex == 4'h1}} & 7'b1111001 |
+					{7{hex == 4'h2}} & 7'b0100100 |
+					{7{hex == 4'h3}} & 7'b0110000 |
+					{7{hex == 4'h4}} & 7'b0011001 |
+					{7{hex == 4'h5}} & 7'b0010010 |
+					{7{hex == 4'h6}} & 7'b0000010 |
+					{7{hex == 4'h7}} & 7'b1111000 |
+					{7{hex == 4'h8}} & 7'b0000000 |
+					{7{hex == 4'h9}} & 7'b0010000 |
+					{7{hex == 4'hA}} & 7'b0001000 |
+					{7{hex == 4'hB}} & 7'b0000011 | 
+					{7{hex == 4'hC}} & 7'b1000110 |
+					{7{hex == 4'hD}} & 7'b0100001 | 
+					{7{hex == 4'hE}} & 7'b0000110 |
+					{7{hex == 4'hF}} & 7'b0001110);
+
+	assign digit = (INVERT) ? temp : ~temp;
+endmodule
+
+module button_handler_down
+#(
+	parameter INVERT = 1
+)
+(
+	input	[0:0]	clock,
+	input	[0:0]	button_signal,
+	output	[0:0]	button_flag
+);
+
+reg [0:0] temp_1;
+reg [0:0] temp_2;
+
+always @(posedge clock)
+begin
+	temp_1 <= button_signal ^ INVERT;
+	temp_2 <= temp_1; 
+end
+
+	assign button_flag = (~temp_2) & temp_1;
+endmodule
